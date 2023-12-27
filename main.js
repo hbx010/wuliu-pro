fd4a12bbfae00eeb0e77c575e6a191451e71dd86
hbx <2282777835@qq.com>
2023-12-27 18:59:22 +0800

    wuliu

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 import App from './App'
 
+// 引入工具库
+import './utils/utils'
+
+// 引入 Pinia
+import { createPinia } from 'pinia'
+
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-  ...App
+	...App
 })
 app.$mount()
 // #endif
@@ -14,9 +20,14 @@ app.$mount()
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+
+	// 实例化Pinia
+	const pinia = createPinia()
+	// 传递给项目应用
+	app.use(pinia)
+	return {
+		app
+	}
 }
 // #endif
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,51 +2,52 @@
 	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
-			<text class="title">{{title}}</text>
+			<text class="title">title</text>
+
+			<button type="default" @click="handleRequest">登录</button>
 		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+<script setup lang="ts">
+import { login } from '@/api/login'
 
-		},
-		methods: {
-
-		}
-	}
+const handleRequest = async () => {
+	uni.utils.toast()
+	const res = await login({
+		account: 'xbsj001',
+		password: '123456'
+	})
+	console.log('res=>', res)
+}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
+.logo {
+	height: 200rpx;
+	width: 200rpx;
+	margin-top: 200rpx;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 50rpx;
+}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+.text-area {
+	display: flex;
+	justify-content: center;
+}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.title {
+	font-size: 36rpx;
+	color: #8f8f94;
+}
 </style>
+	utils: any;
+	[x: string]: any;
